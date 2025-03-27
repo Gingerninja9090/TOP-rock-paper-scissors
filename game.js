cc2dfa3031a0daff53c5552c344bcab097b6c671
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-03-27 14:31:32 +0200

    Added additional JS code created a input for human answer created scores and set human answer to always be lowercase

--- game.js
+++ game.js
@@ -1,23 +1,45 @@
-console.log("Hello World")
-
 // Get computer choice
- //   Randomly generates a number
- //   If less than or equal to 33 then rock
- //   If less than or equal to 66 and greater than 33 then paper
- //   If less than or equal to 99 and greater than 66 then scissors
+ // Randomly generates a number
+ // If less than or equal to 33 then rock
+ // If less than or equal to 66 and greater than 33 then paper
+ // If less than or equal to 99 and greater than 66 then scissors
 
 let randomNumber = Math.floor(Math.random() * 100); // Number range is 0 to 99
 
+console.log(randomNumber) //Outputs random number
+
 let getComputerChoice = " "; // Computers choice
     if (randomNumber <= 99 && randomNumber > 66) {
-        getComputerChoice = "Scissors"
+        getComputerChoice = "scissors"
     }
     else if (randomNumber <= 66 && randomNumber > 33 ) {
-        getComputerChoice = "Paper"
+        getComputerChoice = "paper"
     }
     else if (randomNumber <= 33) {
-        getComputerChoice = "Rock"
+        getComputerChoice = "rock"
     }
 
-console.log(randomNumber)
-console.log(getComputerChoice)
+console.log(getComputerChoice) //Outputs computers choice
+
+// Get human choice
+ // Display prompt with question and text input
+ // Return users input
+
+ let getHumanChoice = prompt("Pick from rock, paper or scissors").toLowerCase()
+
+ console.log(getHumanChoice)
+
+// Get score for players
+ // Set player score
+ // Set computers score
+
+let humanScore = 0
+let computerScore = 0
+
+console.log(humanScore)
+console.log(computerScore)
+
+// Play round of game
+ // Function to activate human and computer choice
+ // Compare outputs and declare a winner
+ // increase winner score
