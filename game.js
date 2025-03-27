6a6290425feb225a353915c0efedf71dad1d28b4
Gingerninja9090 <195748104+Gingerninja9090@users.noreply.github.com>
2025-03-27 12:09:23 +0200

    Created randomly generated computer answer created a random number generator that converts to range of 0 to 99 created a if else statement that uses number range to determine output left in pseudocode for reference and added comments to code

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
 console.log("Hello World")
+
+// Get computer choice
+ //   Randomly generates a number
+ //   If less than or equal to 33 then rock
+ //   If less than or equal to 66 and greater than 33 then paper
+ //   If less than or equal to 99 and greater than 66 then scissors
+
+let randomNumber = Math.floor(Math.random() * 100); // Number range is 0 to 99
+
+let getComputerChoice = " "; // Computers choice
+    if (randomNumber <= 99 && randomNumber > 66) {
+        getComputerChoice = "Scissors"
+    }
+    else if (randomNumber <= 66 && randomNumber > 33 ) {
+        getComputerChoice = "Paper"
+    }
+    else if (randomNumber <= 33) {
+        getComputerChoice = "Rock"
+    }
+
+console.log(randomNumber)
+console.log(getComputerChoice)
